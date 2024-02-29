1a3d65d3c70ea0a5c79e6f7a3269385f00026cd8
manfortun <m.fortun@arcanys.com>
2024-02-29 15:22:50 +0800

    Add automapper

--- Program.cs
+++ Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
