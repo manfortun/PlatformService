8087a8523417814086cf8336da0dde28ab3ac9e1
manfortun <m.fortun@arcanys.com>
2024-02-29 15:34:32 +0800

    Add PlatformsController.cs

---
+++ Controllers/PlatformsController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.DataAccess.Interfaces;
+using PlatformService.DTOs;
+
+namespace PlatformService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    private readonly IPlatformRepository _repository;
+    private readonly IMapper _mapper;
+
+    public PlatformsController(
+        IPlatformRepository platformRepository,
+        IMapper mapper)
+    {
+        _repository = platformRepository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        Console.WriteLine("Getting platforms...");
+
+        var platformItem = _repository.GetAll();
+
+        return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+    }
+}
